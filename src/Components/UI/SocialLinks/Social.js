f419229a475001c98f14196b066714ebe91eaecd
De-marauder <ezikegodson@gmail.com>
2022-01-15 20:11:23 +0100

    Final commit...

--- src/Components/UI/SocialLinks/Social.js
+++ src/Components/UI/SocialLinks/Social.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Social.module.css'
 
 const social = (props) => {
-    
+
     const listStyle = props.type.split(' ')
     let style = ''
     listStyle.forEach((el) => {
@@ -12,10 +12,10 @@ const social = (props) => {
 
     return (
         <ul className={style} style={{ top: props.top }} >
+            <li><a href='https://twitter.com/DecadenTeam?s=20' ><img src="https://img.icons8.com/office/50/000000/twitter.png" alt="" /></a></li>
             <li><img src="https://img.icons8.com/office/50/000000/facebook-new.png" alt="" /></li>
             <li><img src="https://img.icons8.com/office/50/000000/youtube-play.png" alt="" /></li>
             <li><img src="https://img.icons8.com/office/50/000000/instagram-new.png" alt="" /></li>
-            <li><img src="https://img.icons8.com/office/50/000000/twitter.png" alt="" /></li>
         </ul>
     )
 }
